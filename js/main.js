c1f9d153af8177621a32cf6e56f803f8cd38bf7d
lufranski <lufranski@gmail.com>
2022-11-21 14:25:01 +0100

    Milestone 0 + Milestone 1

---
+++ js/main.js
@@ -0,0 +1,53 @@
+// Js - Our Team
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto
+
+// 1. Creare l'array di oggetti
+const teamMembers = [
+    // member #1
+    {
+        'name': 'Wayne Barnett',
+        'role': 'Founder & CEO',
+        'image': 'wayne-barnett-founder-ceo.jpg'
+    },
+    // member #2
+    {
+        'name': 'Angela Caroll',
+        'role': 'Chief Editor',
+        'image': 'angela-caroll-chief-editor.jpg'
+    },
+    // member #3
+    {
+        'name': 'Walter Gordon',
+        'role': 'Office Manager',
+        'image': 'walter-gordon-office-manager.jpg'
+    },
+    // member #4
+    {
+        'name': 'Angela Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela-lopez-social-media-manager.jpg'
+    },
+    // member #5
+    {
+        'name': 'Scott Estrada',
+        'role': 'Developer',
+        'image': 'scott-estrada-developer.jpg'
+    },
+    // member #6
+    {
+        'name': 'Barbara Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+console.log(teamMembers);
+// 2. Stampare su console le info per ogni membro del team
+
+for (let i = 0; i < teamMembers.length; i++){
+    console.log(`Membro #${i}: ${teamMembers[i].name} ${teamMembers[i].role} ${teamMembers[i].image} `);
+}
+
+
+// 3. Stampare su DOM
